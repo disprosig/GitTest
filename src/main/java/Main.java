f0a3222b77ece7d79fd0d5628332bfb1b259c504
disprosig <disprosig@gmail.com>
2017-03-29 12:41:04 +0300

    new commit 2903171240

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by kirill.ivankov on 3/28/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GIT");
+        System.out.println("Second line");
+        System.out.println("Third line");
+
+        Person person = new Person("Max", "Smith", 20);
+    }
+}
